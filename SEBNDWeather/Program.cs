27d2a1b4358e3c34bc0dd148c7c26d59d9449e9e
KappaGnome <ignas.rodzianskis@gmail.com>
2019-04-29 14:23:06 +0300

    Add project files.

---
+++ SEBNDWeather/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace SEBNDWeather
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User, would you like to input data or output data?");
+            Console.WriteLine("1. Input Data");
+            Console.WriteLine("2. Output Data");
+            Console.Write("Selection: ");
+            string Selection = Console.ReadLine();
+            switch (Selection)
+            {
+                case "1":
+                    DataInput();
+                    break;
+
+                case "2":
+                    DataOutput();
+                    break;
+                default:
+                    break;
+            }
+        }
+        public static void DataInput()
+        {
+            
+        }
+        public static void DataOutput()
+        {
+
+        }
+    }
+    
+}
---
+++ SEBNDWeather/WeatherData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEBNDWeather
+{
+    class WeatherData
+    {
+        public string NameOfCity { get; set; }
+        public float Temerature { get; set; }
+        public DateTime DateOfRecord { get; set; }
+        public bool WasPrecipitation { get; set; }
+        public float WindSpeed { get; set; }
+        public List<string> WindDirection { get; set; }
+    }
+}
