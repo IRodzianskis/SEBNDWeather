c25d2c6ab46fbf7a2fdff8a593513c109ee7ceaf
KappaGnome <ignas.rodzianskis@gmail.com>
2019-04-30 18:59:52 +0300

    Split input & output functions to their respective classes and utilized encapsulation.

---
+++ SEBNDWeather/DataOutputClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEBNDWeather
+{
+    public static class DataOutputClass
+    {
+        public static void OutputInit(List<WeatherData> WholeData)
+        {
+            DataOutput(WholeData);
+        }
+        private static void DataOutput(List<WeatherData> WholeData)
+        {
+            Console.WriteLine("========== DATA OUTPUT ==========");
+            Console.WriteLine("Hello User, plase enter your search criteria [Name of City/Date of recording]");
+            Console.Write("Criteria: ");
+            string Selection = Console.ReadLine();
+            Console.Write("\n");
+
+
+            foreach (WeatherData item in WholeData)
+            {
+                Console.WriteLine(item.NameOfCity);
+                Console.WriteLine(item.Temperature);
+                Console.WriteLine(item.DateOfRecord);
+                Console.WriteLine(item.WasPrecipitation);
+                Console.WriteLine(item.WindSpeed);
+                Console.WriteLine(item.WindDirection);
+            }
+        }
+    }
+}
--- SEBNDWeather/Program.cs
+++ SEBNDWeather/Program.cs
@@ -24,95 +24,22 @@ namespace SEBNDWeather
             Console.WriteLine("2. Output Data");
             Console.Write("Selection: ");
             string Selection = Console.ReadLine();
+            var WholeData = new List<WeatherData>();
             switch (Selection)
             {
                 case "1":
-                    DataInput();
+                    WholeData = DataInputClass.InputInit();
+                    UserPromptInitial();
                     break;
 
                 case "2":
-                    DataOutput();
+                    DataOutputClass.OutputInit(WholeData);
+                    UserPromptInitial();
                     break;
                 default:
+                    UserPromptInitial();
                     break;
             }
-        }
-        public static void DataInput()
-        {
-            string City, Direction = "", DateOfRecord = "", PrecipitationCheck = "", Temperature = "", WindSpeed = "";
-            double TemperatureCorrect, WindSpeedCorrect;
-            bool WasPrecipitation;
-
-            Console.WriteLine("========== DATA INPUT ==========");
-            Console.Write("Please input the name of the city: ");
-            City = Console.ReadLine();
-            while (!double.TryParse(Temperature, out TemperatureCorrect))
-            {
-                Console.Write("\nPlease input the temperature in Celsius: ");
-                Temperature = Console.ReadLine();
-            }
-            while (!DateValidation(DateOfRecord))
-            {
-                Console.Write("\nPlease input the date of recording [Format: yyyy/MM/dd]: ");
-                DateOfRecord = Console.ReadLine();
-            }
-            while (PrecipitationCheck.ToLower() != "yes" && PrecipitationCheck.ToLower() != "no")
-            {
-                Console.Write("\nPlease input whether there was precipitation of not [Yes/No]: ");
-                PrecipitationCheck = Console.ReadLine();
-            }
-            if (PrecipitationCheck == "Yes")
-                WasPrecipitation = true;
-            else WasPrecipitation = false;
-            while (!double.TryParse(WindSpeed, out WindSpeedCorrect))
-            {
-                Console.Write("\nPlease input the wind speed [m/s]: ");
-                WindSpeed = Console.ReadLine();
-            }
-            while (!DirectionValidation(Direction))
-            {
-                Console.Write("\nPlease input the direction of wind [Whole or abbreviated directional words (NE, East, eAst etc.)]: ");
-                Direction = Console.ReadLine();
-            }
-            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}", City, TemperatureCorrect, DateOfRecord, WasPrecipitation, WindSpeed, Direction);
-            var WholeData = new List<WeatherData>();
-            WholeData.Add(new WeatherData {
-                NameOfCity = City,
-                Temperature = TemperatureCorrect,
-                DateOfRecord = DateOfRecord,
-                WasPrecipitation = WasPrecipitation,
-                WindSpeed = WindSpeedCorrect,
-                WindDirection = Direction
-            });
-            //foreach (WeatherData item in WholeData)
-            //{
-            //    Console.WriteLine(item.NameOfCity);
-            //    Console.WriteLine(item.Temperature);
-            //    Console.WriteLine(item.DateOfRecord);
-            //    Console.WriteLine(item.WasPrecipitation);
-            //    Console.WriteLine(item.WindSpeed);
-            //    Console.WriteLine(item.WindDirection);
-            //}
-
-        }
-        private static void DataOutput()
-        {
-
-        }
-        private static bool DateValidation(string date)
-        {
-            if (DateTime.TryParseExact(date, "yyyy/MM/dd", null, DateTimeStyles.None, out DateTime Test) == true)
-                return true;
-            else
-                return false;
-        }
-        private static bool DirectionValidation(string direction)
-        {
-            direction = direction.ToLower();
-            List<string> directions = new List<string> { "ne", "e", "se", "s", "sw", "w", "nw", "n", "northeast", "east", "southeast", "south", "southwest", "west", "northwest", "north"};
-            if (directions.Contains(direction.ToLower()))
-                return true;
-            else return false;
         }
     }
     
